3f458a2b87e740fe4a3c4458e59cb7ca32697bb1
jeffski13 <jeffski@okstate.edu>
2017-10-30 22:36:52 -0500

    some styling pulled in for title page

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
+import SampleCompo from './sample-compo';
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <div>React simple starter</div>
+        <SampleCompo />
+      </div>
     );
   }
 }
