4e9263b76df98ef3a18775d193bee7c77f49ab67
jeffski13 <jeffski@okstate.edu>
2018-01-01 18:18:50 -0600

    supports reads from multiple paragraphs

--- client/src/components/BlogPage.js
+++ client/src/components/BlogPage.js
@@ -21,53 +21,64 @@ class BlogPage extends Component {
   }
 
   componentDidMount(){
-    _.delay(()=>{
-      console.log('jeffski delayski');
-      this.props.getBlog1();
-    }, 3000);
-  }
-
-  renderBlogData(){
-    return(
-    <div>
-      <Banner />
-
-      <Grid>
-        <Row className="show-grid blogTitle">
-          <PageHeader>{this.props.currentBlog.title}</PageHeader>
-        </Row>
-        <Row className="show-grid">
-          <div>{this.props.currentBlog.location}</div>
-        </Row>
-        <Row className="show-grid">
-          <div>{this.props.currentBlog.date}</div>
-        </Row>
-        <Row className="show-grid blogPargraph">
-          <Col sm={8} md={4} >{this.props.currentBlog.blogText[0].text}</Col>
-          <Col sm={8} md={4} >
-            <img className="blogimg1" src="/img/chileTripToast.jpg"  height={300} />
-          </Col>
-        </Row>
-        <Row className="show-grid blogPargraph">
-          {this.props.currentBlog.blogText[1]
-          ?<Col sm={8} >{this.props.currentBlog.blogText[1].text}</Col>
-          : null}
-        </Row>
-      </Grid>
-    </div>
-    );
+    this.props.getBlog1();
   }
 
   render(){
     console.log('jeffski, render BlogPage with data: ', this.props.currentBlog);
 
-    //using a property to validate success...bad practice?
+    //using a property to validate success of request...bad practice?
     return (
         <div>
           {this.props.currentBlog.title
           ? ( <div>{this.renderBlogData()}</div> )
           : ( <div>Loading...</div> )}
         </div>
+      );
+    }
+
+    renderBlogData(){
+      //get first paragraph data
+      const firstParagraph =  this.props.currentBlog.blogText[0];
+      //then put the rest of the paragraphs into a separete array
+      const remainingParagraphs = this.props.currentBlog.blogText.slice(1, this.props.currentBlog.blogText.length);
+
+      console.log('jeffski rmain paras: ', remainingParagraphs)
+
+      return(
+      <div>
+        <Banner />
+
+        <Grid>
+          <Row className="show-grid blogTitle">
+            <PageHeader>{this.props.currentBlog.title}</PageHeader>
+          </Row>
+          <Row className="show-grid">
+            <div>{this.props.currentBlog.location}</div>
+          </Row>
+          <Row className="show-grid">
+            <div>{this.props.currentBlog.date}</div>
+          </Row>
+          <Row className="show-grid blogPargraph">
+            <Col sm={8} md={4} >{firstParagraph.text}</Col>
+            <Col sm={8} md={4} >
+              <img className="blogimg1" src="/img/chileTripToast.jpg"  height={300} />
+            </Col>
+          </Row>
+          {remainingParagraphs.length > 0
+            ? remainingParagraphs.map(this.renderRemainingParagraphs)
+            : null}
+        </Grid>
+      </div>
+      );
+    }
+
+    //renders all paragraphs except the first
+    renderRemainingParagraphs(blogTextItem){
+      return(
+        <Row className="show-grid blogPargraph" key={blogTextItem._id}>
+          <Col sm={8} >{blogTextItem.text}</Col>
+        </Row>
       );
     }
 }
