276f6786cf5bd6ceb8f91da9be082d6386ad4933
jeffski13 <jeffski@okstate.edu>
2018-01-02 21:53:56 -0600

    support for bullet unordered list in blog

--- client/src/actions/getblog.js
+++ client/src/actions/getblog.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const GET_BLOG_ACTION='GET_BLOG_ACTION';
-const BLOG_URL = 'http://localhost:3000/blog/1';
+const BLOG_URL = 'http://localhost:3000/blog/6';
 
 export function getBlog1(){
   console.log('jeffski: getting blog');
--- client/src/components/BlogPage.js
+++ client/src/components/BlogPage.js
@@ -18,18 +18,55 @@ class BlogPage extends Component {
     super(props);
 
     this.renderBlogData = this.renderBlogData.bind(this);
+    this.prepareBlogTextData = this.prepareBlogTextData.bind(this);
+    this.renderRemainingParagraphs = this.renderRemainingParagraphs.bind(this);
   }
 
   componentDidMount(){
     this.props.getBlog1();
   }
 
+  prepareBlogTextData(){
+    //
+    let allButFirstParagraphs = this.props.currentBlog.blogText.slice(1, this.props.currentBlog.blogText.length);
+
+    console.log('jeffski prep blog: ',allButFirstParagraphs);
+
+    //get the bullets together in a more consumable format
+    let i;
+    for(i=0; i < allButFirstParagraphs.length; i++){
+      let blogTextItem = allButFirstParagraphs[i];
+
+      //get bullets together in one object (so we can make an unordered list)
+      if(blogTextItem.style === 'bullet'){
+        blogTextItem.bulletText = [];
+        blogTextItem.bulletText.push(allButFirstParagraphs[i].text);
+
+        let j = i+1;
+        while(true){
+          if(allButFirstParagraphs[j].style === 'bullet'){
+            blogTextItem.bulletText.push(allButFirstParagraphs[j].text);
+            allButFirstParagraphs.splice(j, 1);
+          }
+          else{
+            break;
+          }
+        }
+        //at this point the current index of data has a string arr with all the
+        //bullets (bulletText)
+      }
+
+    }
+
+    return allButFirstParagraphs;
+
+  }
+
   renderBlogData(){
     //get first paragraph data
     //then put the rest of the paragraphs into a separete array
     const firstParagraph =  this.props.currentBlog.blogText[0];
-    const remainingParagraphs = this.props.currentBlog.blogText.slice(1, this.props.currentBlog.blogText.length);
-
+    const remainingParagraphs = this.prepareBlogTextData();
     console.log(remainingParagraphs);
 
     return(
@@ -59,18 +96,22 @@ class BlogPage extends Component {
   //renders all paragraphs except the first
   renderRemainingParagraphs(blogTextItem){
     //bulletted list
+    if(blogTextItem.style === "indent "){
+      return(
+        <Row className="show-grid blogPargraph" key={blogTextItem._id}>
+          <Col sm={8} smOffset={1} >
+            {blogTextItem.text}
+          </Col>
+        </Row>
+      );
+    }
+
     if(blogTextItem.style === "bullet"){
+      console.log('jeffski rendering bullet with ', blogTextItem);
       return(
         <Row className="show-grid blogPargraph" key={blogTextItem._id}>
           <Col sm={8} >
-            {blogTextItem.title
-              ? (<ul>
-                  <li><strong>{blogTextItem.title}: </strong>{blogTextItem.text}</li>
-                </ul>)
-              : null }
-          </Col>
-          <Col sm={8} smOffset={1} >
-            {blogTextItem.subText}
+            <ul>{blogTextItem.bulletText.map(this.renderBulletList)}</ul>
           </Col>
         </Row>
       );
@@ -96,6 +137,12 @@ class BlogPage extends Component {
         <Col sm={8} >{blogTextItem.text}</Col>
       </Row>
     );
+  }
+
+  renderBulletList(bulletBlogStrArrItem){
+     return(
+       <li>{bulletBlogStrArrItem}</li>
+     );
   }
 
   render(){
