b3ffce2a652df359f38f4a118f2ad5e4feb2f9ed
jeffski13 <jeffski@okstate.edu>
2018-01-30 16:52:42 -0600

    deleted documentation test files

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,15 +5,11 @@ import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
 import TitlePage from './TitlePage';
 import BlogPage from './BlogPage';
 import Banner from './Banner';
-import DocComp from './DocComp';
 
 //eventually this will do all the routing...later not now,  someday
 export default class App extends Component {
   render() {
     return (
-      <DocComp
-        title="title here"
-      />
       <div>
         <BrowserRouter>
           <div>
