7a9f95040b790321dfd870b56a9fe0d5c1c4586a
jeffski13 <jeffski@okstate.edu>
2018-03-05 21:35:17 -0600

    sample json for aws

--- client/src/components/TitleImg/index.js
+++ client/src/components/TitleImg/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Image from 'react-bootstrap/lib/Image'
 import titleImage from './TitleChileCerroSanCristobal.jpg'
 
-const TitleImg = () =>{
+const TitleImg = () => {
   return(
     <div className='titlePageImg' >
       <Image src={titleImage} responsive />
