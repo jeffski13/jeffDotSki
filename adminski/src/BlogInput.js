25b3aa261ab95fbfcb250f85dcc74dd07476e3f8
jeffski13 <jeffski@okstate.edu>
2018-03-28 22:22:34 -0500

    gitignore dun right

--- adminski/src/BlogInput.js
+++ adminski/src/BlogInput.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import moment from 'moment';
-import {awsApiKey} from './config';
+import {awsApiKey} from './Config/config';
 
 class TitleInput extends Component {
   constructor(props, context) {
