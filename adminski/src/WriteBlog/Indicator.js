136ad2d538ab111bea371acd19a4d00bdebb7ced
jeffski13 <jeffski@okstate.edu>
2018-05-10 20:33:46 -0500

    Indicator clearnup and docs

--- adminski/src/WriteBlog/Indicator.js
+++ adminski/src/WriteBlog/Indicator.js
@@ -4,45 +4,58 @@ import _ from 'lodash';
 
 import './styles.css';
 
+//constants for fade in and fade out transition states
+const STAGE_FADE_IN = 'STAGE_FADE_IN';
+const STAGE_FADE_OUT = 'STAGE_FADE_OUT';
+
+/*
+shows success or failure icon with fade in and fade out animation 
+*/
 class Indicator extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
-            stage: 0
+            transitionStage: null
         }
     }
 
-    componentDidMount(){
+    componentDidMount() {
         //delay fade in
-        _.delay(()=>{
-            this.setState({stage: 1}, this.delayFadeOut);
+        _.delay(() => {
+            this.setState({ transitionStage: STAGE_FADE_IN }, this.fadeOutAfterDelay);
         }, 100);
     }
 
-    delayFadeOut = () =>{
-        _.delay(()=>{
-            this.setState({stage: 0});
+    //will change state to fading out after a few seconds
+    fadeOutAfterDelay = () => {
+        _.delay(() => {
+            this.setState({ transitionStage: STAGE_FADE_OUT });
         }, 2000);
     }
 
     render() {
-        let stageCssClass = [
-            'statusAreaHidden',
-            'statusAreaVisible',
-            'statusAreaHidden'
-        ];
-    
+        //get CSS class for current animation (empty by default)
+        let stageCssClass = '';
+        if (this.state.transitionStage === STAGE_FADE_IN) {
+            stageCssClass = 'statusAreaVisible';
+        }
+        else if (this.state.transitionStage === STAGE_FADE_OUT) {
+            stageCssClass = 'statusAreaHidden';
+        }
+
+        //error case
         let indicatorIconText = 'error';
         let indicatorColorClass = 'failureColor'
-        if(this.props.success){
+        if (this.props.success) {
+            //success case
             indicatorColorClass = 'successColor'
             indicatorIconText = 'done';
         }
 
         return (
-            <div className={`statusArea ${stageCssClass[this.state.stage]}`} >
+            <div className={`statusArea ${stageCssClass}`} >
                 <i className={`material-icons ${indicatorColorClass}`}  >{indicatorIconText}</i>
             </div>
         );
