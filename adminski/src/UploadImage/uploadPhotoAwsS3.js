48dbb0faa73602afc801ae9ced42c8528f893a9f
jeffski13 <jeffski@okstate.edu>
2018-05-11 12:42:20 -0500

    documentation of blog and photo upload methods

--- adminski/src/UploadImage/uploadPhotoAwsS3.js
+++ adminski/src/UploadImage/uploadPhotoAwsS3.js
@@ -1,10 +1,13 @@
 import { AWS_S3_BUCKET_NAME } from '../configski';
 
-//please provide the following:
-// file - photo file
-// tripName - name of the trip for this photo
-// awsClient - s3 sdk object
-// callback(error, data) - function with error/data information from s3
+/**
+ * get all objects in the AWS S3 blogs "directory"
+ *
+ * @param {object} file - photo file
+ * @param {string} tripName - name of the trip for this photo
+ * @param {object} awsS3Client - the s3 sdk object
+ * @param {function} callback - (error, data) - function with error/data information from s3
+ */
 export function uploadPhoto(file, tripName, awsS3client, callback) {
     let fileName = file.name;
     let blogImageUploadKey = `blog/${tripName}/${fileName}`;
@@ -22,10 +25,12 @@ export function uploadPhoto(file, tripName, awsS3client, callback) {
     });
 }
 
-//get all objects in the AWS S3 blogs "directory"
-// s3bucket - the name of the s3 bucket we need to access
-// awsS3Client - the s3 sdk object
-// callback(error, blogObjectsArr) - a function that will have the blog objects (or an error)
+/**
+ * get all objects in the AWS S3 blogs "directory"
+ *
+ * @param {object} awsS3Client - the s3 sdk object
+ * @param {function} callback - (error, blogObjectsArr) - a function that will have the blog objects (or an error)
+ */
 export function fetchBlogObjects(awsS3Client, callback) {
     let s3Params = { Bucket: AWS_S3_BUCKET_NAME };
     awsS3Client.listObjects(s3Params, (err, data) => {
