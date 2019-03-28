00c35d5e36d55238e2dfcf8418c90274a407352b
jeffski13 <jeffskiosu@gmail.com>
2019-03-28 18:29:02 -0500

    Traveler's Trails home page looking good on mobile

--- src/TravelTrails/Home/index.js
+++ src/TravelTrails/Home/index.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import {Col} from 'react-bootstrap';
+import { Container, Col, Row, Image } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import {jeffskiRoutes} from '../../app';
+import { jeffskiRoutes } from '../../app';
 
 import './styles.css';
 
 export default class Home extends React.Component {
     render() {
         return (
-            <div id="travelTrailsHome">
-                <div className="TravelTrailsHome_titleContainer">
-                    <h1 className="TravelTrailsHome_title">Welcome To<br/>Traveler's Trails</h1>
-                    <Link to={jeffskiRoutes.login} id="travelTrailHome-buttonLeft" className="TravelTrail_button TravelTrail_button-full">LOGIN</Link>
-                    <Link to={jeffskiRoutes.profile} id="travelTrailHome-buttonLeft" className="TravelTrail_button TravelTrail_button-ghost">PROFILE</Link>
-                </div>
-            </div>
+            <Container fluid className="TravelTrailsHome_container">
+                <Row noGutters={true} >
+                    <Col className="d-none d-sm-block">
+                        <div id="travelTrailsHome">
+                            <div className="TravelTrailsHome_titleContainer">
+                                <h1 className="TravelTrailsHome_title">Welcome To<br />Traveler's Trails</h1>
+                                <Link to={jeffskiRoutes.login} id="travelTrailHome-buttonLeft" className="TravelTrail_button TravelTrail_button-full">LOGIN</Link>
+                                <Link to={jeffskiRoutes.profile} id="travelTrailHome-buttonLeft" className="TravelTrail_button TravelTrail_button-ghost">PROFILE</Link>
+                            </div>
+                        </div>
+                    </Col>
+                    <Col className="d-block d-sm-none TravelTrailers_mobile">
+                        <Image src="https://s3.us-east-2.amazonaws.com/jeff.ski/traveltrail/home/traveltrail_home_md.jpg" />
+                        <div className="TravelTrailsHome_titleContainer TravelTrailers_mobile-text">
+                            <h1 className="TravelTrailsHome_title">Welcome To<br />Traveler's Trails</h1>
+                            <Link to={jeffskiRoutes.login} id="travelTrailHome-buttonLeft" className="TravelTrail_button TravelTrail_button-full">LOGIN</Link>
+                            <Link to={jeffskiRoutes.profile} id="travelTrailHome-buttonLeft" className="TravelTrail_button TravelTrail_button-ghost">PROFILE</Link>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
