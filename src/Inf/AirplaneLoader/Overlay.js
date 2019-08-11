0ab510d3d49ef645332d467cab48d65bee2a8081
jeffski13 <jeffskiosu@gmail.com>
2019-08-11 16:49:52 -0500

    Some fancy transitions for the loading plane

--- src/Inf/AirplaneLoader/Overlay.js
+++ src/Inf/AirplaneLoader/Overlay.js
@@ -3,10 +3,26 @@ import AirplaneLoader from './index';
 
 import './overlay-styles.css';
 
+const LOADING_CLASS_BASE = 'loading-overlayski';
+const LOADING_CLASS_TRANSITION = 'loading-overlayski-transition';
+
 class Overlay extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loadingClasses: LOADING_CLASS_BASE
+        };
+    }
+    componentDidMount() {
+        this.setState({
+            loadingClasses: `${LOADING_CLASS_BASE} ${LOADING_CLASS_TRANSITION}`
+        });
+    }
+
     render() {
         return (
-            <div id="loading-overlayski">
+            <div className={this.state.loadingClasses}>
                 <AirplaneLoader />
             </div>
         );
