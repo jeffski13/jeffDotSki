777b78d3b561371bd894ec1eb04e298c7b45e379
Jeff Szcinski II <jeffskiosu@gmail.com>
2025-06-26 14:11:43 -0500

    deployment working

--- copyToBuildFirebase.cjs
+++ copyToBuildFirebase.cjs
@@ -1,7 +1,8 @@
 const path = require('path');
 const ncp = require('ncp').ncp;
+const fs = require('fs');
 
-let srcDir = path.join(__dirname, "build");
+let srcDir = path.join(__dirname, "build/client");
 
 let destDir = path.join(__dirname, "firebaseski/public");
 
@@ -9,11 +10,33 @@ var ncpoptions = {
     "clobber": true
 }
 
-ncp(srcDir, destDir, ncpoptions, function (err) {
-    if (err) {
-        return console.error(err);
+// delete everything inside of the destination directory
+const deleteOldFilesAndcopyNewFiles = async () => {
+    try {
+        await deleteOldFiles(destDir);
+
+        ncp(srcDir, destDir, ncpoptions, function (err) {
+            if (err) {
+                return console.error(err);
+            }
+            else{
+                console.log(`directory ncp copy done for ${srcDir} to ${destDir}`);
+            }
+        });
     }
-    else{
-        console.log('directory ncp copy done!');
+    catch (err) {
+        console.error('Error during initial setup: ' + err);
     }
-});
+};
+
+const deleteOldFiles = async (dirForDeletingFiles) => {
+    console.log('Deleting old files in ' + dirForDeletingFiles);
+    //check that the directory exists
+    if (!fs.existsSync(dirForDeletingFiles)) {
+        console.log('Directory does not exist, nothing to delete.');
+        return;
+    }
+    fs.rmSync(dirForDeletingFiles, { recursive: true })
+};
+
+deleteOldFilesAndcopyNewFiles();
