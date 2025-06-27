2bee1c7c9306474b2357033a0c079743ea00d00d
Jeff Szcinski II <jeffskiosu@gmail.com>
2025-06-27 13:07:32 -0500

    refactor for the copying of files to deploy

--- copyToBuildFirebase.cjs
+++ copyToBuildFirebase.cjs
@@ -2,41 +2,52 @@ const path = require('path');
 const ncp = require('ncp').ncp;
 const fs = require('fs');
 
-let srcDir = path.join(__dirname, "build/client");
-
-let destDir = path.join(__dirname, "firebaseski/public");
-
-var ncpoptions = {
-    "clobber": true
-}
-
 // delete everything inside of the destination directory
 const deleteOldFilesAndcopyNewFiles = async () => {
     try {
-        await deleteOldFiles(destDir);
-
-        ncp(srcDir, destDir, ncpoptions, function (err) {
-            if (err) {
-                return console.error(err);
-            }
-            else{
-                console.log(`directory ncp copy done for ${srcDir} to ${destDir}`);
-            }
-        });
+        const srcDir = path.join(__dirname, "build/client");
+        const destDir = path.join(__dirname, "firebaseski/public");
+        await deleteAllFilesInDirectory(destDir);
+        
+        await copySourceFiles(srcDir, destDir)
+        console.log('Files copied successfully from ' + srcDir + ' to ' + destDir);
     }
     catch (err) {
-        console.error('Error during initial setup: ' + err);
+        console.error('Error during file copy process: ' + err);
     }
 };
 
-const deleteOldFiles = async (dirForDeletingFiles) => {
-    console.log('Deleting old files in ' + dirForDeletingFiles);
+/**
+ * @param {string} srcDir 
+ * @param {string} destDir
+ */
+const copySourceFiles = (srcDir, destDir) => {
+    const ncpoptions = {
+        "clobber": true
+    }
+    return new Promise((resolve, reject) => {
+        ncp(srcDir, destDir, ncpoptions, function (err) {
+            if (err) {
+                console.error(`Error copying files from ${srcDir} to ${destDir}:`, err);
+                reject(err);
+            }
+            else{
+                console.log(`Directory ncp copy done from ${srcDir} to ${destDir}`);
+                resolve();
+            }
+        });
+    });
+}
+
+const deleteAllFilesInDirectory = async (directory) => {
+    console.log(`Deleting all files in ${directory}...`);
     //check that the directory exists
-    if (!fs.existsSync(dirForDeletingFiles)) {
+    if (!fs.existsSync(directory)) {
         console.log('Directory does not exist, nothing to delete.');
         return;
     }
-    fs.rmSync(dirForDeletingFiles, { recursive: true })
+    fs.rmSync(directory, { recursive: true })
+    console.log(`All files in ${directory} deleted.`);
 };
 
 deleteOldFilesAndcopyNewFiles();
