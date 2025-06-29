aba78dc0ff6343e8ee189ea5c5f1a34c60ebebde
Jeff Szcinski II <jeffskiosu@gmail.com>
2025-06-28 20:04:31 -0500

    all unit tests passing with condensed test data

--- app/Pokeperu/selection/PokePeruContent.test.tsx
+++ app/Pokeperu/selection/PokePeruContent.test.tsx
@@ -1,46 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import {PokePeruContent} from '../index';
-import { ElementType } from '../ElementType';
-import type { Monster } from '../monsters';
-
-const mockSelectedMonsters: Monster[] = [
-  {
-    name: 'Pikachu',
-    trainer: 'Ash',
-    description: '',
-    hp: 35,
-    attack: 55,
-    defense: 40,
-    specialAttack: 50,
-    specialDefense: 50,
-    speed: 90,
-    type: ElementType.Electric,
-    secondType: null,
-    image: '/images/monsters/pikachu.jpg',
-    attack1: { name: 'Quick Attack', damage: 10, type: ElementType.Normal, isPhysical: true, powerPoints: 20, accuracy: 1 },
-    attack2: { name: 'Thunderbolt', damage: 20, type: ElementType.Electric, isPhysical: false, powerPoints: 20, accuracy: 1 },
-    trainerImage: '',
-    inspiration: ''
-  },
-  {
-    name: 'Charmander',
-    trainer: 'Brock',
-    description: '',
-    hp: 47,
-    attack: 52,
-    defense: 43,
-    specialAttack: 60,
-    specialDefense: 50,
-    speed: 65,
-    type: ElementType.Fire,
-    secondType: null,
-    image: '/images/monsters/charmander.jpg',
-    attack1: { name: 'Scratch', damage: 10, type: ElementType.Normal, isPhysical: true, powerPoints: 20, accuracy: 1 },
-    attack2: { name: 'Flamethrower', damage: 20, type: ElementType.Fire, isPhysical: false, powerPoints: 20, accuracy: 1 },
-    trainerImage: '',
-    inspiration: ''
-  },
-];
+import mockSelectedMonsters from '../mockMonsters';
 
 describe('PokePeruStart Component', () => {
   test('handleMonsterSelect modifies the selected monsters', () => {
