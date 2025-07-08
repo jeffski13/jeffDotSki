86e7bf6ae9a73aa6dd905c22051ca3391d38c24e
Jeff Szcinski II <jeffskiosu@gmail.com>
2025-07-08 14:54:27 -0400

    scaffolding for running automated unit tests (#65)
    * scaffolding for running automated unit tests
    
    * test to see if unit tests fail and stop in cloud env
    
    * running unit tests on PR update
    
    * Update unit-test-action.yml
    
    * fixing unit tests

--- app/Pokeperu/battle/Battle.test.tsx
+++ app/Pokeperu/battle/Battle.test.tsx
@@ -29,12 +29,12 @@ mocksWithMonster1VeryWeak[1].attack1 = {
   accuracy: 1,
 }
 
-const testMonster1Hp = (hpExpected) => {
+const testMonster1Hp = (hpExpected: Number) => {
   const hpValueMonster1 = document.querySelector('.hp-value-monster1');
   const monster1Hp = parseInt(hpValueMonster1?.innerHTML);
   expect(monster1Hp).toBe(hpExpected);
 }
-const testMonster2Hp = (hpExpected) => {
+const testMonster2Hp = (hpExpected: Number) => {
   const hpValueMonster2 = document.querySelector('.hp-value-monster2');
   const monster2Hp = parseInt(hpValueMonster2?.innerHTML);
   expect(monster2Hp).toBe(hpExpected);
