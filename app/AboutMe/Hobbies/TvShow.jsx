fb466da10360a6971fb0291c2856e7fa68312d1d
Jeff Szcinski II <jeffskiosu@gmail.com>
2025-07-08 14:19:30 -0400

    Hobbies drawings (#62)
    * modernize Hobbies react code
    
    * updating TV Shows
    
    * first drawing appearing
    
    * other drawings
    
    * added halloween items and cleaned up CSS

--- app/AboutMe/Hobbies/TvShow.jsx
+++ app/AboutMe/Hobbies/TvShow.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Container, Row, Col } from 'react-bootstrap';
 import './styles.css';
 
 class TvShow extends React.Component {
@@ -8,37 +7,25 @@ class TvShow extends React.Component {
 
     let seasonRender = null;
     if (this.props.season) {
-      seasonRender = (<div className="tvshowInfo" >
-        <div className="tvshowTitle" >Season:</div>
-        <div className="tvshowText" >{this.props.season}</div>
+      seasonRender = (<div className="hobbieItemInfo" >
+        <div className="hobbieItemTitle" >Season:</div>
+        <div className="hobbieItemText" >{this.props.season}</div>
       </div>
       );
     }
 
     return (
-
-      <div className="TvShow" >
-        <Container>
-          <Row className="show-grid">
-            <Col xs={0} sm={2} />
-            <Col xs={8} sm={2} >
-              <div className="tvshowImageContainer" >
-                <img className="tvshowImage" src={this.props.thumb} alt={`${this.props.title} Show`} />
-              </div>
-            </Col>
-            <Col xs={12} sm={4} >
-              <div className="tvshowInfoContainer">
-                <div className="tvshowInfo" >
-                  <div className="tvshowTitle" >Show:</div>
-                  <div className="tvshowText" >{this.props.title}</div>
-                </div>
-                {seasonRender}
-              </div >
-            </Col>
-            <Col xs={0} sm={4} />
-            <Col xs={0} sm={2} />
-          </Row>
-        </Container>
+      <div className="HobbieContentItem" >
+        <div className="hobbieImageContainer" >
+          <img className="hobbieImage tvshowImage" src={this.props.thumb} alt={`${this.props.title} Show`} />
+        </div>
+        <div className="hobbieItemInfoContainer">
+          <div className="hobbieItemInfo" >
+            <div className="hobbieItemTitle" >Show:</div>
+            <div className="hobbieItemText" >{this.props.title}</div>
+          </div>
+          {seasonRender}
+        </div >
       </div>
     );
   }
--- app/AboutMe/Hobbies/index.jsx
+++ app/AboutMe/Hobbies/index.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
 import TvShow from './TvShow';
+import drawings from './hobbies-drawings.json';
+import drawingsHalloween from './hobbies-drawings-halloween.json';
 import tvshows from './hobbies-tvshows.json';
 import tvshowsFinished from './hobbies-tvshows-finished.json';
 import '../styles.css';
 import './styles.css';
 
-class Hobbies extends React.Component {
-
-  renderTvShows(tvshowItem, index) {
-    return (
+const renderTvShows = (tvshowItem, index) => {
+  return (
+    <Col xs={12} sm={6} md={4}>
       <li key={index} >
         <TvShow
           title={tvshowItem.title}
@@ -18,78 +18,117 @@ class Hobbies extends React.Component {
           thumb={tvshowItem.thumb}
         />
       </li>
-    )
-  }
-
-  render() {
-    return (
-      <div className="aboutmeWrapper">
-        <div className="shotGlassTextSection" >
-          <Container>
-            <Row className="show-grid">
-              <Col sm={1}>
-              </Col>
-              <Col sm={11}>
-                <div className="aboutMeTitle" >Hobbies & Interests</div>
-              </Col>
-            </Row>
-          </Container>
-          <Container className="hobbiesSectionText">
-            <Row className="show-grid hobbiesSection">
-              <Col sm={1}>
-              </Col>
-              <Col sm={11}>
-                <div className="hobbiesSectionTitle">TV Shows:</div>
-              </Col>
-            </Row>
-            <Row className="show-grid">
-              <Col sm={1}>
-              </Col>
-              <Col sm={11}>
-                <div>Over the years I have found that I connect with media and digital art on an emotional and spritual level.
-                  I watch one episode every night before I go to bed.
-                  I have found these shows over the years to be a constant comfort no matter what stage of life.
-                  Here are some things I am currently watching.</div>
-              </Col>
-            </Row>
-            <Row className="show-grid">
-              <Col sm={2}>
-              </Col>
-              <Col sm={10}>
-              </Col>
-            </Row>
-          </Container>
-          <ul className="tvshowList" >
-            {tvshows.map(this.renderTvShows)}
-          </ul>
-
-          <Container className="hobbiesSectionText">
-            <Row className="show-grid hobbiesSection">
-              <Col sm={1}>
-              </Col>
-              <Col sm={11}>
-                <div className="hobbiesSectionTitle">Past Shows:</div>
-              </Col>
-            </Row>
-            <Row className="show-grid">
-              <Col sm={1}>
-              </Col>
-              <Col sm={11}>
-                <div>And here are the shows I have finished (Possibly more than once!). I enojoyed the heck out of every one of these.</div>
-              </Col>
-            </Row>
-          </Container>
-          <ul className="tvshowList" >
-            {tvshowsFinished.map(this.renderTvShows)}
-          </ul>
-        </div>
-      </div>
-    );
-  }
+    </Col>
+  );
 }
 
-Hobbies.propTypes = {
+const renderDrawings = (drawingItem, index) => {
+  return (
+    <Col xs={12} md={6}>
+      <li key={index} >
+        <div className="hobbieItemInfoContainer">
+          <div className="hobbieItemInfo" >
+            <div className="hobbieItemTitle" >Title:</div>
+            <div className="hobbieItemText" >{drawingItem.name}</div>
+          </div>
+        </div >
+        <div className="HobbieContentItem" >
+          <div className="hobbieImageContainer" >
+            <img className="hobbieImage drawingImage" src={drawingItem.thumb} alt={`${drawingItem.name} Drawing`} />
+          </div>
+        </div>
+      </li>
+    </Col>
+  );
+}
 
-};
+export default function Hobbies() {
+  return (
+    <div className="aboutmeWrapper">
+      <div className="hobbiesSection" >
+        <Container>
+          <Row>
+            <Col sm={12}>
+              <div className="aboutMeTitle" >Hobbies & Interests</div>
+            </Col>
+          </Row>
+        </Container>
 
-export default Hobbies;
+        <Container className="hobbiesSection">
+          <Row>
+            <Col sm={12}>
+              <div className="hobbiesSectionTitle">Drawings:</div>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={12} className="hobbiesSectionText">
+              <div>I started drawing as a hobby during the pandemic. Over the years I feel I have found my style. Enjoy!</div>
+            </Col>
+          </Row>
+          <ul className="hobbiesContentList" >
+            <Row>
+              {drawings.map(renderDrawings)}
+            </Row>
+          </ul>
+        </Container>
+
+        <Container className="hobbiesSection">
+          <Row>
+            <Col sm={12}>
+              <div className="hobbiesSectionTitle">Spooky Drawings:</div>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={12} className="hobbiesSectionText">
+              <div>My family likes the spooky nightmare before christmas style, so I created these: </div>
+            </Col>
+          </Row>
+          <ul className="hobbiesContentList" >
+            <Row>
+              {drawingsHalloween.map(renderDrawings)}
+            </Row>
+          </ul>
+        </Container>
+
+        <Container className="hobbiesSection">
+          <Row>
+            <Col sm={12}>
+              <div className="hobbiesSectionTitle">TV Shows:</div>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={12} className="hobbiesSectionText">
+              <div>Over the years I have found that I connect with media and digital art on an emotional and spritual level.
+                I watch one episode every night before I go to bed.
+                I have found these shows over the years to be a constant comfort no matter what stage of life.
+                Here are some things I am currently watching.</div>
+            </Col>
+          </Row>
+          <ul className="hobbiesContentList" >
+            <Row>
+              {tvshows.map(renderTvShows)}
+            </Row>
+          </ul>
+        </Container>
+
+        <Container className="hobbiesSection">
+          <Row className="show-grid">
+            <Col sm={12}>
+              <div className="hobbiesSectionTitle">Past Shows:</div>
+            </Col>
+          </Row>
+          <Row>
+            <Col sm={12} className="hobbiesSectionText">
+              <div>And here are the shows I have finished (Possibly more than once!). I enojoyed the heck out of every one of these.</div>
+            </Col>
+          </Row>
+          <ul className="hobbiesContentList" >
+            <Row>
+              {tvshowsFinished.map(renderTvShows)}
+            </Row>
+          </ul>
+        </Container>
+      </div>
+    </div>
+  );
+}
