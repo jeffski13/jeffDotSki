fd381500ba0922d1016e6b5ae759fb63c486d141
Jeff Szcinski II <jeffskiosu@gmail.com>
2025-07-08 15:47:24 -0400

    Update TitleImg.tsx (#66)

--- app/TitlePage/TitleImg.tsx
+++ app/TitlePage/TitleImg.tsx
@@ -7,7 +7,7 @@ export default function TitleImg() {
   // );
   return (
     <div className="TitleImage" >
-      <Image src="https://s3.us-east-2.amazonaws.com/jeff.ski/title/TitleChileCerroSanCristobal-md.jpg" fluid />
+      <Image src="https://s3.us-east-2.amazonaws.com/jeff.ski/title/jeff_villarica_horse_2025.jpg" fluid />
     </div>
   );
 }
