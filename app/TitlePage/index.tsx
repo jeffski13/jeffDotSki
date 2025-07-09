23bd698a4d086b8880a52e38a82a5fe0b8444358
Jeff Szcinski II <jeffskiosu@gmail.com>
2025-07-09 17:51:39 -0400

    title image mobile friendly (#72)

--- app/TitlePage/index.tsx
+++ app/TitlePage/index.tsx
@@ -1,5 +1,3 @@
-
-import TitleImg from "./TitleImg";
 import { Container, Row, Col, Image } from "react-bootstrap";
 import './styles.css';
 import FooterBarski from "~/Inf/FooterBarski";
@@ -8,18 +6,20 @@ import ROUTES from "~/consts/ROUTES";
 export default function TitlePage() {
   return (
     <div className="TitlePage" >
-      <TitleImg />
+      <div>
+        <div className="TitleImage" >
+          <Image src="https://s3.us-east-2.amazonaws.com/jeff.ski/title/jeff_villarica_horse_2025_1080.jpg" fluid />
+        </div>
+        <h1 className="TitlePage_title-container"><span className="TitlePage_title-websiteName">JEFF</span> <span className="TitlePage_title-nonwebsitename">SZCIN</span><span className="TitlePage_title-websiteName">SKI</span></h1>
+      </div>
       <Container fluid className="TitlePage_aboutJeff">
         <Row>
-          <Col xs={12} sm={12} className="TitlePage_aboutJeff-name" title="Jeff Szcinski">
-            <h2><span className="TitlePage_aboutJeff-name-websiteName">JEFF</span> <span className="TitlePage_aboutJeff-name-nonwebsitename">SZCIN</span><span className="TitlePage_aboutJeff-name-websiteName">SKI</span></h2>
-          </Col>
           <Col xs={12} sm={12} md={5} className="TitlePage_aboutJeff-image">
             <Image src="https://s3.us-east-2.amazonaws.com/jeff.ski/title/titlePage-info1-md.jpg" roundedCircle fluid />
           </Col>
           <Col xs={12} sm={12} md={7} className="TitlePage_aboutJeff-text">
             <div className="TitlePage_aboutJeff-text-container">
-              <h3>Who Is This Jeff Guy?</h3>
+              <h3>Who Is Jeff Szcinski (Jeffski)?</h3>
               <ul>
                 <li><strong><a className="titlePageLink" href={ROUTES.pokePeru.info}>English Education Innovator</a>:</strong> Elementry School and Private Lessons for Spanish Speakers</li>
                 <li><strong><a className="titlePageLink" href={ROUTES.external.resume.softwareEngineer}>Software Engineer</a>:</strong> Web Developer and Android Native Applications</li>
