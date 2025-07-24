3a46720fbdefa2c2d9d948cd36dd60d443dd47b0
Jeff Szcinski II <jeffskiosu@gmail.com>
2025-07-24 12:40:42 +0900

    gym leader biomes

--- app/Pokeperu/gymleaders.ts
+++ app/Pokeperu/gymleaders.ts
@@ -10,16 +10,19 @@ export const gymLeaders: GymLeader[] = [
     name: "???",
     image: "/images/gymleaders/trainersquad01.png",
     environmentImage: "/images/perulandscape/peru-9.jpg",
+    biome: "Mordor",
   },
   {
     name: "Jim",
     image: "/images/gymleaders/trainersquad02.png",
     environmentImage: "/images/perulandscape/peru-10.png",
+    biome: "Nether",
   },
   {
     name: "Tom",
     image: "/images/gymleaders/trainersquad03.png",
     environmentImage: "/images/perulandscape/peru-11.jpg",
+    biome: "Forest",
   },
   {
     name: "Lily",
