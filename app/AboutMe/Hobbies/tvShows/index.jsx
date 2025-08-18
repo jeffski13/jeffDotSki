27487d7aa032b995704bc344044677bce31eaa17
Jeff Szcinski II <jeffskiosu@gmail.com>
2025-08-18 09:44:59 +0900

    added evangelion to anime list

--- app/AboutMe/Hobbies/tvShows/index.jsx
+++ app/AboutMe/Hobbies/tvShows/index.jsx
@@ -28,12 +28,14 @@ export default function TvShows() {
       intro: 'A lo largo de los años he descubierto que conecto con los medios y el arte digital a nivel emocional y espiritual. Veo un episodio cada noche antes de dormir. Estas series han sido una constante en mi vida. Estas son las que estoy viendo hoy en día.',
       past: 'Series Pasadas:',
       pastDesc: 'Y aquí están las series que ya terminé (posiblemente más de una vez). A mi me encantaba cada una de ellas.',
+      disclaimer: 'Yo no soy el dueño de este contenido. Por favor, apoya los productos oficiales.'
     },
     default: {
       title: 'Tv Shows',
       intro: 'Over the years I have found that I connect with media and digital art on an emotional and spritual level. I watch one episode every night before I go to bed. I have found these shows over the years to be a constant comfort no matter what stage of life. Here are some things I am currently watching.',
       past: 'Past Shows:',
       pastDesc: 'And here are the shows I have finished (Possibly more than once). I enojoyed the heck out of every one of these.',
+      disclaimer: "I don't own any of these images. Please support the official releases."
     }
   };
   const content = getContentByLanguage(multiLangContent, getBrowserLanguage());
@@ -74,6 +76,11 @@ export default function TvShows() {
               {tvshowsFinished.map(renderTvShows)}
             </Row>
           </ul>
+          <Row className="show-grid BioHobbiesImg">
+            <Col sm={8} smOffset={1} className="bioHobbieDisclaimer" >
+              <p>*{content.disclaimer}</p>
+            </Col>
+          </Row>
         </Container>
       </div>
     </div>
