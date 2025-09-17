d8669b80c2978f15bd279303de3c040b1bc3bd3c
Jeff Szcinski II <jeffskiosu@gmail.com>
2025-09-17 16:12:17 +0900

    unit test for testing existence of battle route in dex

--- app/Pokeperu/pokedex/Pokedex.test.tsx
+++ app/Pokeperu/pokedex/Pokedex.test.tsx
@@ -5,11 +5,20 @@ import mockSelectedMonsters from '../mockMonsters';
 describe('Pokedex Component', () => {
   it('displays the monster name when rendered', () => {
     // Render the Pokedex component
-    render(<Pokedex selectedMonsters={mockSelectedMonsters} battleRoute='' />);
+    render(<Pokedex selectedMonsters={mockSelectedMonsters} battleRoute="/battle" />);
 
     // Assert that each monster's name is displayed
     mockSelectedMonsters.forEach((monster) => {
       expect(screen.getAllByText(monster.name).length).toBe(2);
     });
+  });
+
+  it('renders the battleRoute link', () => {
+    const battleRoute = '/battle';
+    render(<Pokedex selectedMonsters={mockSelectedMonsters} battleRoute={battleRoute} />);
+    // Look for a link with the correct href
+    const link = screen.getByRole('link', { name: 'Back' });
+    expect(link).toBeInTheDocument();
+    expect(link).toHaveAttribute('href', battleRoute);
   });
 });
