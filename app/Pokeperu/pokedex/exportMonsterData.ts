34a88dc89273a15beb7b6b4a7f324cd99fbe7e65
Jeff Szcinski II <jeffskiosu@gmail.com>
2025-09-25 13:30:53 +0900

    defaults for attacks to be balanced on export

--- app/Pokeperu/pokedex/exportMonsterData.ts
+++ app/Pokeperu/pokedex/exportMonsterData.ts
@@ -1,56 +1,51 @@
 import type { Monster } from "../monsters";
 
-export const getMonsterData = (editData, selectedMonsters: Monster[]) => {
-return new Promise((resolve, reject) => {
+const balancedDefaultAttack = {
+    damage: 40,
+    powerPoints: 4,
+    accuracy: 0.75,
+};
 
-    const allData = Object.keys(editData).map(monsterName => {
-        const monster = selectedMonsters.find(monster => monster.name === monsterName);
-        if (!monster) return null;
-        const edit = editData[monsterName];
-        // Merge edits into monster
-        const merged = {
-            ...monster,
-            ...edit,
-            attack1: { ...monster.attack1, ...(edit.attack1 || {}) },
-            attack2: { ...monster.attack2, ...(edit.attack2 || {}) },
-        };
-        // Only include specified properties
-        const {
-            name: mergedName,
-            trainer,
-            trainerImage,
-            hp,
-            attack,
-            defense,
-            specialAttack,
-            specialDefense,
-            speed,
-            type,
-            secondType,
-            image,
-            description,
-            inspiration,
-            attack1,
-            attack2
-        } = merged;
-        resolve({
-            name: mergedName,
-            trainer,
-            trainerImage,
-            hp,
-            attack,
-            defense,
-            specialAttack,
-            specialDefense,
-            speed,
-            type,
-            secondType,
-            image,
-            description,
-            inspiration,
-            attack1,
-            attack2
-        });
-    }).filter(Boolean);
-})
+export const getMonsterData = (editData, selectedMonsters: Monster[]) => {
+    return new Promise((resolve, reject) => {
+        Object.keys(editData).map(monsterName => {
+            const monster = selectedMonsters.find(monster => monster.name === monsterName);
+            if (!monster) {
+                return null
+            };
+            const edit = editData[monsterName];
+            // Merge edits into monster
+            const merged = {
+                ...monster,
+                ...edit,
+                attack1: { ...monster.attack1, ...(edit.attack1 || {...balancedDefaultAttack}) },
+                attack2: { ...monster.attack2, ...(edit.attack2 || {...balancedDefaultAttack}) },
+            };
+            //recreate the Monster object
+            const {
+                name: mergedName,
+                trainer,
+                trainerImage,
+                type,
+                secondType,
+                image,
+                description,
+                inspiration,
+                hp, attack, defense, specialAttack, specialDefense, speed,
+                attack1,attack2
+            } = merged;
+            resolve({
+                name: mergedName,
+                trainer,
+                trainerImage,
+                type,
+                secondType,
+                image,
+                description,
+                inspiration,
+                hp, attack, defense, specialAttack, specialDefense, speed,
+                attack1, attack2
+            });
+        }).filter(Boolean);
+    })
 }
