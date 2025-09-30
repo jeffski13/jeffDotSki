9b374519ec99932496a90b45ef70849f782bce71
Jeff Szcinski II <jeffskiosu@gmail.com>
2025-09-30 16:49:05 +0900

    simplified the exporting of the monsters as it does work, it just doesnt have the pretty enum typing in the syntax

--- app/Pokeperu/pokedex/exportMonsterData.ts
+++ app/Pokeperu/pokedex/exportMonsterData.ts
@@ -1,5 +1,4 @@
 import type { Monster } from "../monsters";
-import { ElementType } from "../ElementType";
 
 const balancedDefaultAttack = {
     name: "Tackle",
@@ -8,25 +7,6 @@ const balancedDefaultAttack = {
     damage: 40,
     powerPoints: 4,
     accuracy: 0.75,
-};
-
-// Transform types to ElementType
-const toElementTypeString = (val): string | null => {
-    console.log(`in: ${val}`);
-    if (val == null) {
-        console.log(`returning null`);
-        return null;
-    };
-    if (Object.values(ElementType).includes(val)) {
-        console.log(`returning val as ElementType`);
-        return `ElementType.${val as ElementType}`
-    }
-    // fallback: try to match string
-    const found = Object.values(ElementType).find(e => e.toLowerCase() === String(val).toLowerCase());
-    if(found) {
-        return `ElementType.${found}`
-    }
-    return null;
 };
 
 /**
@@ -67,19 +47,19 @@ export const getMonsterData = (editData, selectedMonsters: Monster[]) => {
                 name: mergedName,
                 trainer,
                 trainerImage,
-                type: toElementTypeString(type),
-                secondType: toElementTypeString(secondType),
+                type: type,
+                secondType: secondType,
                 image,
                 description,
                 inspiration,
                 hp, attack, defense, specialAttack, specialDefense, speed,
                 attack1: {
                     ...attack1,
-                    type: toElementTypeString(attack1.type)
+                    type: attack1.type
                 },
                 attack2: {
                     ...attack2,
-                    type: toElementTypeString(attack2.type)
+                    type: attack2.type
                 }
             }
             console.log('finalMonsterExportData: ', finalMonsterExportData)
