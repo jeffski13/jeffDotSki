28c277193cb7aee7a22ff641f47bc67c58541ddf
Jeff Szcinski II <jeffskiosu@gmail.com>
2025-10-13 08:51:45 -0500

    gym leaders unit tests

--- app/Pokeperu/info/InfoPage.test.tsx
+++ app/Pokeperu/info/InfoPage.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import InfoPageContainer from './InfoPage';
+import { monsters } from '../monsters';
+import { gymLeaders } from '../gymleaders';
 
 describe('Pokedex Component', () => {
   it('renders the back link', () => {
@@ -17,11 +19,18 @@ describe('Pokedex Component', () => {
     expect(gymTextarea).toBeDefined();
     const value = gymTextarea!.value;
     // Check each property from the GymLeader interface is present in the template string
-    expect(value).toMatch(/id:\s*'/);
-    expect(value).toMatch(/name:\s*"/);
-    expect(value).toMatch(/image:\s*"/);
-    expect(value).toMatch(/environmentImage:\s*"/);
-    expect(value).toMatch(/biome:\s*"/);
+    const objectWithAllProperties = gymLeaders[0];
+
+    const expectedPatterns: Array<RegExp> = [];
+    Object.keys(objectWithAllProperties).forEach(key => {
+      let flags = "s";
+      let regex = new RegExp(key, flags);
+      expectedPatterns.push(regex)
+    });
+
+    expectedPatterns.forEach((pattern) => {
+      expect(value).toMatch(pattern);
+    });
   });
 
   it('shows all Monster properties inside the pokemon blank data UI', () => {
@@ -32,22 +41,19 @@ describe('Pokedex Component', () => {
     const pokemonTextarea = textareas.find(t => t.value.includes('trainerId') || t.value.includes('attack1'));
     expect(pokemonTextarea).toBeDefined();
     const value = pokemonTextarea!.value;
+    
     // Check each property from the Monster interface is present in the template string
-    expect(value).toMatch(/id:\s*'/);
-    expect(value).toMatch(/name:\s*'/);
-    expect(value).toMatch(/trainerId:\s*'/);
-    expect(value).toMatch(/hp:\s*10/);
-    expect(value).toMatch(/attack:\s*10/);
-    expect(value).toMatch(/defense:\s*10/);
-    expect(value).toMatch(/specialAttack:\s*10/);
-    expect(value).toMatch(/specialDefense:\s*10/);
-    expect(value).toMatch(/speed:\s*10/);
-    expect(value).toMatch(/type:\s*ElementType\.Normal/);
-    expect(value).toMatch(/secondType:\s*ElementType\.Normal/);
-    expect(value).toMatch(/image:\s*''/);
-    expect(value).toMatch(/description:\s*''/);
-    expect(value).toMatch(/attack1:\s*\{/);
-    expect(value).toMatch(/attack2:\s*\{/);
-    expect(value).toMatch(/inspiration:\s*''/);
+    const objectWithAllProperties = monsters[0];
+
+    const expectedPatterns: Array<RegExp> = [];
+    Object.keys(objectWithAllProperties).forEach(key => {
+      let flags = "s";
+      let regex = new RegExp(key, flags);
+      expectedPatterns.push(regex)
+    });
+
+    expectedPatterns.forEach((pattern) => {
+      expect(value).toMatch(pattern);
+    });
   });
 });
