046fca9e85246c758d9d4edde59bf92d7dd80968
Jeff Szcinski II <jeffskiosu@gmail.com>
2025-11-04 14:50:10 -0300

    Minor version bump option (#140)
    * minor version flag
    
    * test
    
    * 2.0.127
    
    * fixing issue foor minor patch bug
    
    * 2.1.0
    
    * 2.1.1
    
    * reverting npm
    
    * remove debug code

--- deployski.sh
+++ deployski.sh
@@ -1,8 +1,32 @@
+#!/bin/bash
 executionScriptDir=$(pwd)
 
-echo "howdy y'all, lets deploy this thang"
+echo "Howdy, let's deploy this thing!"
 
-npm version patch
+if [ "$1" == "-h" ]; then
+  echo "Deployment script"
+  echo "Flags:"
+  echo "    -m minor version npm patch (by default normal patch)"
+  exit 0
+fi
+
+#get all CLI args
+flagsProcessed=0
+isMinorPatch=false
+for var in "$@"
+do
+  if [[ "$var" = "-m" ]]; then
+    isMinorPatch=true
+  fi
+  flagsProcessed=$((flagsProcessed+1))
+done
+
+if [ "$isMinorPatch" = true ] ; then
+  npm version minor
+else
+  npm version patch
+fi
+
 npm run build
 npm run copyBuildToFirebase
 
