e4a3a8a1814b603ad71a6a9acc05d76a49a0742a
Jeff Szcinski II <jeffskiosu@gmail.com>
2025-11-18 10:02:13 -0300

    links for info page

--- app/pokePeruApp/info/InfoPage.test.tsx
+++ app/pokePeruApp/info/InfoPage.test.tsx
@@ -1,17 +1,18 @@
 import { render, screen } from '@testing-library/react';
-import InfoPageContainer from './InfoPage';
+import InfoPageContainer, { InfoPage } from './InfoPage';
 import { monsters } from '../monsters';
 import { gymLeaders } from '../gymleaders';
+import { locationProviderMock } from '~/aboutMeSection/PortfolioProps';
 
 describe('Pokedex Component', () => {
   it('renders the back link', () => {
-    render(<InfoPageContainer />);
+    render(<InfoPage isTestEnv={true} locationProvider={locationProviderMock} />);
     const link = screen.getByRole('button', { name: 'Go back' });
     expect(link).toBeInTheDocument();
   });
 
   it('shows all GymLeader properties inside the gym leader blank data UI', () => {
-    render(<InfoPageContainer />);
+    render(<InfoPage isTestEnv={true} locationProvider={locationProviderMock} />);
     // There are two copyable textareas on the page (pokemon and gym leader). Find the one for gym leader by looking for a unique key.
     const textareas = screen.getAllByRole('textbox') as HTMLTextAreaElement[];
     expect(textareas.length).toBeGreaterThanOrEqual(2);
@@ -34,7 +35,7 @@ describe('Pokedex Component', () => {
   });
 
   it('shows all Monster properties inside the pokemon blank data UI', () => {
-    render(<InfoPageContainer />);
+    render(<InfoPage isTestEnv={true} locationProvider={locationProviderMock} />);
     // There are two copyable textareas on the page (pokemon and gym leader). Find the one for pokemon by looking for a unique key.
     const textareas = screen.getAllByRole('textbox') as HTMLTextAreaElement[];
     expect(textareas.length).toBeGreaterThanOrEqual(2);
