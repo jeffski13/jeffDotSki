c35e2bed8e10257a51e976c3f5621ed133b98855
Jeff Szcinski II <jeffskiosu@gmail.com>
2025-11-24 14:51:45 -0300

    background image path fix and unit tests (#173)

--- app/pokePeruApp/pokeperuExtendedUniverse/southCarolina/backgroundImagesExtendedUniverse.ts
+++ app/pokePeruApp/pokeperuExtendedUniverse/southCarolina/backgroundImagesExtendedUniverse.ts
@@ -1,9 +1,6 @@
 import backgroundImageList from "~/pokePeruApp/battle/backgroundImagesList";
 
 export const backgroundImageListExtendedUniverse: string[] = [
-  '/images/extendedUniverse/claremore/landscape/claremore-9.jpg',
-  '/images/extendedUniverse/claremore/landscape/claremore-10.jpg',
-  '/images/extendedUniverse/claremore/landscape/claremore-11.jpg',
 ];
 
 export const getExtendedUniverseBackgroundImages = (): string[] => {
