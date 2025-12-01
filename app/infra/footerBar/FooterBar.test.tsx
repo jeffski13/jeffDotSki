857bb0bf7ddc735504c7c46ed9ff37c3a7a2e967
Jeff Szcinski II <jeffskiosu@gmail.com>
2025-12-01 15:45:28 -0300

    made by section for the website (#179)

--- app/infra/footerBar/FooterBar.test.tsx
+++ app/infra/footerBar/FooterBar.test.tsx
@@ -8,12 +8,18 @@ describe('FooterBar Component', () => {
     expect(screen.getByText(`Version: ${packageJson.version}`)).toBeInTheDocument();
   });
 
+  test('renders the made by section', () => {
+    render(<FooterBar />);
+    expect(screen.getByText(/Built with /)).toBeInTheDocument();
+    expect(screen.getByText(/by Jeff Szcinski/)).toBeInTheDocument();
+  });
+
   test('displays the correct version number', () => {
-      render(<FooterBar />);
-      
-      // Check that the specific version number is displayed
-      const versionElement = screen.getByText(/Version: \d+\.\d+\.\d+/);
-      expect(versionElement).toBeInTheDocument();
-      expect(versionElement.textContent).toBe(`Version: ${packageJson.version}`);
-    });
+    render(<FooterBar />);
+
+    // Check that the specific version number is displayed
+    const versionElement = screen.getByText(/Version: \d+\.\d+\.\d+/);
+    expect(versionElement).toBeInTheDocument();
+    expect(versionElement.textContent).toBe(`Version: ${packageJson.version}`);
+  });
 });
