a3079c87b44920b4b232d569ddd80ea96476d765
Jeff Szcinski II <jeffskiosu@gmail.com>
2025-12-11 13:26:57 -0300

    removed for dark mode

--- app/infra/NavigationBar.tsx
+++ app/infra/NavigationBar.tsx
@@ -64,7 +64,7 @@ export default function NavigationBar({ themeManager }: NavigationBarProps) {
         themeManager.setupDarkMode();
     }, []);
 
-    const darkModeButtonText = (theme === THEME.DARK) ? 'light' : 'dark';
+    const darkModeButtonText = (theme === THEME.DARK) ? 'Light Mode' : 'Dark Mode';
     const darkModeButtonIcon = (theme === THEME.DARK) ? '/images/darkMode/dark-mode-light.png' : '/images/darkMode/dark-mode-dark.png';
 
     return (
