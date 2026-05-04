68212e59d8bcba05f58f17f54f0d240a6e989d03
Jeff Szcinski II <jeffskiosu@gmail.com>
2026-05-04 14:09:07 -0500

    trashcan moved to expanded settings (#234)

--- app/readingsNihonDe/index.tsx
+++ app/readingsNihonDe/index.tsx
@@ -530,25 +530,27 @@ export default function ReadingsNihonDe() {
               <span>設定 (Settings)</span>
               <span className="readingsNihonDe-settings-caret">{settingsOpen ? '▲' : '▼'}</span>
             </button>
-            <button
-              className="readingsNihonDe-settings-reset"
-              onClick={() => setConfirmResetOpen(true)}
-              title="Reset settings"
-              aria-label="Reset settings"
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
-              </svg>
-            </button>
           </Col>
 
           {settingsOpen && (
             <Col xs={12} className="readingsNihonDe-settings-row">
               <div className="readingsNihonDe-settings-panel">
-                <p className="readingsNihonDe-settings-hint">
-                  Toggle rows on/off and drag to reorder.
-                </p>
+                <div className="readingsNihonDe-settings-panel-header">
+                  <p className="readingsNihonDe-settings-hint">
+                    Toggle rows on/off and drag to reorder.
+                  </p>
+                  <button
+                    className="readingsNihonDe-settings-reset"
+                    onClick={() => setConfirmResetOpen(true)}
+                    title="Reset settings"
+                    aria-label="Reset settings"
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                      <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                    </svg>
+                  </button>
+                </div>
                 <ul className="readingsNihonDe-settings-list">
                   {displayOrder.map((key) => {
                     const opt = DISPLAY_OPTIONS.find((o) => o.key === key)!;
